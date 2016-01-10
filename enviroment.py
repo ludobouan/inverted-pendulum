f0b6af612b2cdfa872cc11b8dae1041aa81c1151
totolef <totolef@gmail.com>
2016-01-10 15:16:24 +0100

    Arduino et environnement
    Ajout du code arduino
    Modification de l'environnement pour communiquer en Serial

--- enviroment.py
+++ enviroment.py
@@ -13,10 +13,13 @@ import os
 import DbManager
 import logging
 import sys
+import serial
 
 # ******************
 # *** Global Var ***
 # ******************
+
+ser = serial.Serial('COM3', 115200)
 
 # *******************
 # ***** CLasses *****
@@ -30,17 +33,24 @@ class env:
         self.speed = get_speed()
         self.state = get_state()
         self.reward = get_reward()
+        while true:
+            self.read_serial()
 
     def get_reward(self):
         return(-self.angle^2)
-
-    def get_angle(self):
-        pass
-
-    def get_speed(self):
-        pass
+        
+    def read_serial(self):
+        line = ser.readline()
+        if not line == "":
+            self.angle = line // 10000
+            self.speed = line % 1000
 
     def get_state(self):
+        return self.angle / 30 + (self.speed * 0.2 + 20) / 100
+        # partie entiere, position (entre -10 et 10) et decimal vitesse angulaire (0 et 40)
+
+    def take_action(self, value):
+        ser.write(value+"\n")
 
 
 # *******************
